c9418b843b80c2916126adb94815e043e7afcc77
akshaygautam <akshay.gautam2209@gmail.com>
2023-11-19 03:02:03 +0530

    new update

--- src/main/java/com/akshaygautam/azureapps/azureapps/controller/BaseController.java
+++ src/main/java/com/akshaygautam/azureapps/azureapps/controller/BaseController.java
@@ -1,5 +1,8 @@
 package com.akshaygautam.azureapps.azureapps.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +15,6 @@ import java.util.*;
 public class BaseController {
 
 
-
     @GetMapping("/sayHello")
     public String sayHello() {
         return "Hello there ! this is Akshay Gautam's app";
@@ -20,6 +22,19 @@ public class BaseController {
 
     @GetMapping("/sayMyName/{myName}")
     public String sayMyName(@PathVariable String myName) {
-        return "Hello "+ myName + " How are you, This is Akshay Gautam's App";
+        return "Hello " + myName + " How are you, This is Akshay Gautam's App";
+    }
+
+    @GetMapping("/tableOf/{number}")
+    public ResponseEntity<List<String>> multiplicationTable(@PathVariable Integer number) {
+        List<String> table = new ArrayList<>();
+
+        for (int i = 0; i <= 10; i++) {
+            StringBuilder entry = new StringBuilder();
+            entry.append(number).append(" x ").append(i).append(" = ").append(number * i);
+            table.add(entry.toString());
+        }
+
+        return new ResponseEntity<>(table, HttpStatus.OK);
     }
 }
