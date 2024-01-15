ca69d2c0e94a90fde8710b070444dfece4754aa3
akshaygautam <akshay.gautam2209@gmail.com>
2024-01-15 23:10:06 +0530

    added front end

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Base from "./basecontroller/Base";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Learning Full stack</h1>
+        <Base tableId="5"></Base>
+        <Base tableId="8"></Base>
+        <Base tableId="9"></Base>
+        <Base tableId="24"></Base>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/basecontroller/Base.jsx
@@ -0,0 +1,50 @@
+import React, {Component} from "react";
+
+class Base extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null,
+            loading: true,
+            error: null,
+        };
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData = async () => {
+        const { tableId } = this.props;
+        try {
+            const response = await fetch(`http://localhost:8080/akshaygautam/tableOf/${tableId}`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            this.setState({ data, loading: false });
+        } catch (error) {
+            this.setState({ error, loading: false });
+        }
+    };
+    render() {
+        const { data, loading, error } = this.state;
+
+        if (loading) {
+            return <p>Loading...</p>;
+        }
+
+        if (error) {
+            return <p>Error: {error.message}</p>;
+        }
+
+        return (
+            <div>
+                <h1>Data:</h1>
+                <pre>{JSON.stringify(data, null, 2)}</pre>
+            </div>
+        );
+    }
+}
+
+export default  Base;
